9d8de036526e4bed286d3c31da2d83aa045af5e9
Rajat Gupta <rajat29gupta@gmail.com>
2020-10-01 17:53:07 +0530

    Added SPA using Angular 10
    Feature Available
    => add new todo
    => style
    => valid HTML template
    => complete/uncomplete todo

---
+++ todo_Angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material-module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    DemoMaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
