e5e0feee9c16cdde05ead9a5831660fd886987df
soosa <anett.soos1066@gmail.com>
2020-10-01 20:59:51 +0100

    todo_RHW added OpenSans fonts

--- todo_RHW/webpack.config.js
+++ todo_RHW/webpack.config.js
@@ -40,7 +40,21 @@ module.exports = {
               },
             },
           ],
-      }],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ],  
   },
   plugins: [htmlPlugin]
 };
+
