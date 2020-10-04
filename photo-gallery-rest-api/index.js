ac9c0cb4681eabc4ff01e22f45117df4262bec3a
soosa <anett.soos1066@gmail.com>
2020-10-04 18:58:29 +0100

    deleted unused repo

--- photo-gallery-rest-api/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('build'))
-app.get("/*", (req, res) => res.sendFile(__dirname + '/build/index.html'))
-app.listen(3000, () => console.log('Server running on port 3000'))
--- photo-gallery-rest-api/src/js/header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Header = () =>
-  <header>
-  	<h1> The Bubble Witch Saga </h1>
-  </header>;
-
-export default Header;
